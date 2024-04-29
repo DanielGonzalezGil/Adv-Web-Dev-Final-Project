3ea7222c593931276d1255048e4068e2f59fd56b
Daniel <126022409+DanielGonzalezGil@users.noreply.github.com>
2024-04-28 23:23:32 -0400

    Got it to display DB info

--- mydiary/Diary/models.py
+++ mydiary/Diary/models.py
@@ -5,13 +5,15 @@ from django.contrib.auth.models import User
 
 
 class DiaryEntry(models.Model):
-    user = models.ForeignKey(
+    created_by = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="diary_entries"
     )
     title = models.CharField(max_length=100)
     content = models.TextField()
-    date_posted = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
+
+
     def __str__(self):
-        return f'{self.title} by {self.user.username} on {self.date_posted.strftime("%Y-%m-%d")}'
+        return f'{self.title} by {self.created_by} on {self.created_on.strftime("%Y-%m-%d")}'
--- mydiary/Diary/urls.py
+++ mydiary/Diary/urls.py
@@ -3,13 +3,6 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    # TODO: Make sure that the URL paths below point to the correct corresponding views
-    path(
-        "login/",
-        auth_views.LoginView.as_view(template_name="diary/login.html"),
-        name="login",
-    ),
-    path("logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
-    path("register/", views.register_view, name="register"),
+    path("", views.base, name="base"),
+    
 ]
