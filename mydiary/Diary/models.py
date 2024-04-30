a139f037233006fec002a2cd675823639b6e9d19
Daniel <126022409+DanielGonzalezGil@users.noreply.github.com>
2024-04-30 11:56:02 -0400

    tried to adjust the bootstrap card layout, it aint it cheif

--- mydiary/Diary/models.py
+++ mydiary/Diary/models.py
@@ -13,7 +13,5 @@ class DiaryEntry(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
-
-
     def __str__(self):
         return f'{self.title} by {self.created_by} on {self.created_on.strftime("%Y-%m-%d")}'
